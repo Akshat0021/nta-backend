5c5dbab4e09e12f97f6e2e1bcaef3a0acb597106
Akshat Kumar <128487278+Akshat0021@users.noreply.github.com>
2025-04-17 15:55:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import urllib.request
 import os
 
 app = Flask(__name__)
+CORS(app)  # 👈 Allow all cross-origin requests
 
 @app.route('/')
 def home():
@@ -13,11 +15,11 @@ def run_tracker():
     logs = []
     for j in range(1000):
         logs.append(str(j))
-        for i in range(42,100):
+        for i in range(100):
             if i < 10:
                 url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/202504170{i}.pdf"
             else:
-                url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/20250411{i}.pdf"
+                url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/20250417{i}.pdf"
             try:
                 response = urllib.request.urlopen(url)
                 code = response.getcode()
@@ -28,5 +30,5 @@ def run_tracker():
     return jsonify({"logs": logs, "success": False})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render uses dynamic port
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
