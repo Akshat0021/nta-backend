501690f7b370fedba0b808eaa1b44cbc85e82855
Akshat Kumar <128487278+Akshat0021@users.noreply.github.com>
2025-04-18 14:47:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ def tracker_loop():
                 if i == 49:
                     continue
                 current_i = i
-                logs.append(f"⏳ Running tracker... (i = {i})")
+                logs.append(f"⏳ Running tracker... ({i}/99)")
                 if i < 10:
-                    url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/202504170{i}.pdf"
+                    url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/202504180{i}.pdf"
                 else:
-                    url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/20250417{i}.pdf"
+                    url = f"https://cdnbbsr.s3waas.gov.in/s3f8e59f4b2fe7c5705bf878bbd494ccdf/uploads/2025/04/20250418{i}.pdf"
                 try:
                     response = urllib.request.urlopen(url)
                     code = response.getcode()
